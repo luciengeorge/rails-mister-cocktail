fd0093d31a54a1776b53413676840334385d441c
Lucien George <luciengeorge95@gmail.com>
2018-11-09 14:59:52 +0000

    style improved, added reviews controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root to: 'cocktails#index'
   resources :cocktails, only: %i[index show new create destroy] do
     resources :doses, only: %i[create]
+    resources :reviews, only: %i[create]
   end
   resources :doses, only: %i[destroy]
+  resources :reviews, only: %i[destroy]
 end
